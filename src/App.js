c3164272b7f71878c4bc8b29e63aa59e9b33f13d
sonipooja2023 <122546638+sonipooja2023@users.noreply.github.com>
2023-01-12 14:21:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import DataTable from './DataTable' ;
-import { Container} from 'reactstrap';
 
 function App() {
   return (
-   <div className="App">
- <Container fluid>
-          <h1 className="display-3">WELCOME TO BOOTCAMP - Jan 10 2023 Edition</h1>
-          <p className="lead">My Name Pooja Soni  </p>
-          <p className="lead"><a href="https://github.com/sonipooja2023">Here</a> is my Github Link  </p>
-          <p className="lead"><a href="http://webdevnotes.wynisco.com">Here</a> Here is Class Notes Link </p>
-          <p className="lead"><a href="http://webdevhw.wynisco.com">Here</a> Here is Homeworks Link </p>
-          <p className="lead"><a href="https://docs.google.com/spreadsheets/d/1I75Kt78QSCRSAiF74yBttNyWVI4bq2ONsTWnN_ai9iA/edit?usp=sharing">Here</a> Main Google Spreadsheet </p>
-
-     <DataTable />
-     </Container>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
